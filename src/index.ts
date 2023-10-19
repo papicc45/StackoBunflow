873667c16d493f4bc9688d9cc445051db98ca6a9
papicc45 <papicc45@naver.com>
2023-10-19 09:23:36 +0900

    cors

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,9 @@ import {
 
 const client = new PrismaClient();
 const setup = ( app : Elysia ) => app.decorate('db', client);
-const corsOptions = {
-    origin: 'https://web-stackobunflow-client-jvpb2alnv5joz1.sel5.cloudtype.app/', // 허용하려는 도메인
-    optionsSuccessStatus: 200
-}
+
 const app = new Elysia()
-    .use(cors(corsOptions))
+    .use(cors())
     .use(setup)
     .use(jwt({ name : 'jwt', secret : 'elysiaApplicationSecretKey' }))
     .use(cookie())
